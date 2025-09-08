8bf36ba775012baeecc1eb781f05e18b1a6c2ae4
guilhermelevii <guilhermelevii.gl@gmail.com>
2025-09-08 17:28:21 -0300

    work

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,17 +1,22 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BancoBrasilComponent } from '../banco-brasil/banco-brasil';
-import { SicoobComponent } from '../sicoob/sicoob';
-
+import { CommonModule } from '@angular/common'; // Necessário para *ngIf
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'; // Para [(ngModel)] e reactive forms
+import { BancoBrasilComponent } from '../banco-brasil/banco-brasil'; // Componente filho
+import { SicoobComponent } from '../sicoob/sicoob'; // Componente filho
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule,FormsModule, ReactiveFormsModule, BancoBrasilComponent, SicoobComponent],
+  imports: [
+    CommonModule,             // Para diretivas como *ngIf
+    FormsModule,              // Para [(ngModel)]
+    ReactiveFormsModule,      // Se quiser usar forms reativos depois
+    BancoBrasilComponent,     // Importando os componentes usados no HTML
+    SicoobComponent
+  ],
   templateUrl: './home.html',
   styleUrls: ['./home.css']
 })
 export class HomeComponent {
-  bancoSelecionado: string = '';
+  bancoSelecionado: string = ''; // Variável usada no select [(ngModel)]
 }
