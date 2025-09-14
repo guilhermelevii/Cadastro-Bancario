597c4eb30cfea80db71cb46ee311d0b799cb9600
guilhermelevii <guilhermelevii.gl@gmail.com>
2025-09-14 10:14:12 -0300

    regex-mask

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,3 @@
-// import { bootstrapApplication } from '@angular/platform-browser';
-// import { appConfig } from './app/app.config';
-// import { App } from './app/app';
-
-// bootstrapApplication(App, appConfig)
-//   .catch((err) => console.error(err));
-
-
-// // Arquivo de entrada principal que inicializa o módulo principal da aplicação.
-
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideNgxMask } from 'ngx-mask';
 import { appConfig } from './app/app.config';
